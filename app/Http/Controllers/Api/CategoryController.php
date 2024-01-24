3e9050570571d97e739e3add81fa612925325e42
Novi Aditya <novi@mile.app>
2024-01-25 00:20:12 +0700

    set laravel vue pagination

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::get();
+        $categories = Category::paginate(10);
 
         return CategoryResource::collection($categories);
     }
--- resources/js/components/Categories/Index.vue
+++ resources/js/components/Categories/Index.vue
@@ -10,16 +10,19 @@
             <tbody>
                 <tr v-for="category in categories.data" :key="category.id">
                     <td>{{ category.name }}</td>
-                    <td>{{category.created_at}}</td>
-                    </tr>
+                    <td>{{ category.created_at }}</td>
+                </tr>
             </tbody>
+
         </table>
+        <Bootstrap5Pagination :data="categories" @pagination-change-page="getCategories" :limit="5"/>
     </div>
 </template>
 
 <script setup>
 import { onMounted } from "vue";
 import useCategories from "../../composables/categories";
+import { Bootstrap5Pagination } from 'laravel-vue-pagination';
 
 const { categories, getCategories } = useCategories()
 onMounted(() => {
