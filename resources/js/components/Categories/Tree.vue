0a8887c1a61f4a439d76ca1e866161bf1b74b37b
Novi Aditya <novi@mile.app>
2024-01-25 03:56:43 +0700

    recursive vue

---
+++ resources/js/components/Categories/Tree.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul>
+        <li v-for="subcategory in children" :key="subcategory.id">
+            {{ subcategory.name }}
+            <span v-if="subcategory.item != null" class="bg-success">
+                <ul>
+                    <li><h4>{{subcategory.item.name }} <span class="px-4">${{ subcategory.item.price }}</span></h4></li>
+                </ul>
+            </span>
+            <Tree :children="subcategory.children" :indent="newIndent" :item="subcategory.item"></Tree>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+
+import { ref, computed } from 'vue'
+
+const props = defineProps({
+    indent: {
+        type: Number
+    },
+    item: {
+        type: Object
+    },
+    children: {
+        type: Object
+    },
+})
+
+const indent = ref(105)
+
+const newIndent = computed(() => {
+    return indent.value
+})
+
+const style = computed(() => {
+    return {
+        'margin-left': '55px'
+    }
+})
+</script>
---
+++ resources/js/components/Menus/MenuIndex.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="category in categories.data" :key="category.id">
+                {{ category.name }}
+                <span v-if="category.item != null" class="bg-success">
+                    <ul>
+                        <li>
+                            <h4>{{ category.item.name }} <span class="px-4">${{ category.item.price }}</span></h4>
+                        </li>
+                    </ul>
+                </span>
+                <Tree :children="category.children" :indent="newIndent" />
+            </li>
+        </ul>
+
+        <!-- <table class="table table-hover">
+                <tr v-for="category in categories.data" :key="category.id">
+                    <div v-if="category.parent_id == 0">
+                        <div>
+                            {{ category.name }}
+                            <span v-if="category.item != null" class="bg-success">
+                                <br />
+                                {{ category.item.name }}
+                                <span>${{
+                                    category.item.price }}</span>
+
+                            </span>
+                        </div>
+                        <Tree :subcategories="subcategory.children"></Tree>
+                    </div>
+                </tr>
+            </table> -->
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref, computed } from "vue";
+import useCategories from "../../composables/categories";
+import Tree from "../Categories/Tree.vue";
+
+const { categories, getCategories } = useCategories()
+onMounted(() => {
+    getCategories()
+})
+
+
+const indent = ref(55)
+
+const newIndent = computed(() => {
+    return indent.value
+})
+
+const style = computed(() => {
+    return {
+        'margin-left': '25px'
+    }
+})
+</script>
