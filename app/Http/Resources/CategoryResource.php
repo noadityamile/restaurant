698c1d2cf9d44f5b7285c297e4fb4f57ae47c6b9
Novi Aditya <novi@mile.app>
2024-01-25 00:04:29 +0700

    Category API and DB

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'parent_id' => $this->parent_id,
+            'created_at' => $this->created_at->toDateString(),
+            'children' => $this->children,
+            'discount' => $this->discount,
+            'item' => $this->item
+        ];
+    }
+}
