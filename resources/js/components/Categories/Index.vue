5f78e77df939f16ce19057b4b7d5c12efadcdfd7
Novi Aditya <novi@mile.app>
2024-01-24 23:49:36 +0700

    create categories index component

---
+++ resources/js/components/Categories/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <table class="table table-striped table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Categories</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <th>Test1</th>
+                    <th>Desc1</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
