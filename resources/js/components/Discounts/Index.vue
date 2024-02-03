f2373794f901cd1959060d4a45d5fcc744cc21da
Novi Aditya <novi@mile.app>
2024-02-04 03:13:48 +0700

    add discount button

--- resources/js/components/Discounts/Index.vue
+++ resources/js/components/Discounts/Index.vue
@@ -1,47 +1,47 @@
 <template>
+    <router-link to="/discounts/create" class="mb-2 btn btn-primary">+ Add Discount</router-link>
+
     <div>
-        <div>
-            <table class="table table-striped table-hover table-bordered">
-                <thead>
-                    <tr>
-                        <th>
-                            No
-                        </th>
-                        <th>
-                            Discount Name
-                        </th>
-                        <th>
-                            Scheme
-                        </th>
-                        <th>
-                            Applied Item/Category
-                        </th>
-                        <th>
-                            Percentage
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(discount, index) in discounts.data">
-                        <td>
-                            {{ index +1 }}
-                        </td>
-                        <td>
-                            {{ discount.discount_name }}
-                        </td>
-                        <td>
-                            {{ discount.scheme }}
-                        </td>
-                        <td>
-                            {{ discount.target }}
-                        </td>
-                        <td>
-                            {{ discount.percentage }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <table class="table table-striped table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>
+                        No
+                    </th>
+                    <th>
+                        Discount Name
+                    </th>
+                    <th>
+                        Scheme
+                    </th>
+                    <th>
+                        Applied Item/Category
+                    </th>
+                    <th>
+                        Percentage
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(discount, index) in discounts.data">
+                    <td>
+                        {{ index + 1 }}
+                    </td>
+                    <td>
+                        {{ discount.discount_name }}
+                    </td>
+                    <td>
+                        {{ discount.scheme }}
+                    </td>
+                    <td>
+                        {{ discount.target }}
+                    </td>
+                    <td>
+                        {{ discount.percentage }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
