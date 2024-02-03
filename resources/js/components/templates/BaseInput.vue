dd7016806bc188190d329387f9f09f788299b134
Novi Aditya <novi@mile.app>
2024-02-04 03:11:02 +0700

    create reusable form input

---
+++ resources/js/components/templates/BaseInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mt-4">
+        <label>
+            {{ label }}
+        </label>
+        <input v-bind="$attrs" class="form-control" :placeholder="label" :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)">
+        <div class="text-danger" v-for="message in errors">
+            {{ message }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    label: {
+        type: String,
+        default: ''
+    },
+    modelValue: {
+        type: [String, Number],
+        default: ''
+    },
+    errors: {
+        type: Array,
+        default: () => []
+    }
+})
+</script>
---
+++ resources/js/components/templates/SelectOption.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mt-4">
+        <label>
+            {{ label }}
+        </label>
+        <select v-bind="$attrs" class="form-select" v-model="selectedOption"
+            @change=" $emit('update:modelValue', selectedOption)">
+            <option :value="initial" disabled>{{ initial }}</option>
+            <option v-for="option in options" :key="option.value" :value="option.id">
+                {{ option.name }}
+            </option>
+        </select>
+        <div class="text-danger" v-for="message in errors">
+            {{ message }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue';
+
+const { label, options, modelValue, errors, initial } = defineProps(['label', 'options', 'modelValue', 'errors', 'initial']);
+
+const selectedOption = ref(modelValue);
+
+</script>
